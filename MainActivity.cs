aeade109a6772fd10b4fd8be91b3573aef1f474d
koyickan <32719009+koyickan@users.noreply.github.com>
2018-03-02 10:06:07 -0600

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Day_9_1_PopUpMenuDemo
+{
+    [Activity(Label = "Day_9_1_PopUpMenuDemo", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
